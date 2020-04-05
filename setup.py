dc3cc5631acb3f24f4855cb7848ad5414bdee5fd
NV-27 <38594288+NV-27@users.noreply.github.com>
2020-04-05 22:57:11 +0300

    [UPDATE] Updated setup.py, update requirements.txt path

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
-import os
-import os.path
 from setuptools import setup, find_packages
 
-def find_requires():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    requirements = []
-    with open("{0}/requirements.txt".format(dir_path), "r") as reqs:
-        requirements = reqs.readlines()
-    return requirements    
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
 
 setup(
     name="dspl",
     packages=["dspl"],
     version="0.0.5",
     include_package_data=True,
-    install_requires=find_requires(),
+    install_requires=required,
     author="Nikita Varganov",
     author_email="nikita.varganov.ml@gmail.com",
     license="MIT",
@@ -22,7 +16,6 @@ setup(
     url="https://github.com/NV-27/dspl",
     download_url="https://github.com/NV-27/dspl/archive/0.0.5.tar.gz",
     keywords=["dspl", "ds_platform", "ds_template"],
-    #long_description=readme,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.6",
