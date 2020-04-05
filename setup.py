21226c7a9bcab16521cd7c0f120ebe44f7b2f16b
NV-27 <38594288+NV-27@users.noreply.github.com>
2020-04-05 22:21:52 +0300

    [UPDATE] Updated setup.py, delete readme.md

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ import os
 import os.path
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
-    readme = f.read()
-
 def find_requires():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     requirements = []
@@ -15,7 +12,7 @@ def find_requires():
 setup(
     name="dspl",
     packages=["dspl"],
-    version="0.0.4",
+    version="0.0.5",
     include_package_data=True,
     install_requires=find_requires(),
     author="Nikita Varganov",
@@ -23,9 +20,9 @@ setup(
     license="MIT",
     description="Library for using in DS-Platform",
     url="https://github.com/NV-27/dspl",
-    download_url="https://github.com/NV-27/dspl/archive/0.0.4.tar.gz",
+    download_url="https://github.com/NV-27/dspl/archive/0.0.5.tar.gz",
     keywords=["dspl", "ds_platform", "ds_template"],
-    long_description=readme,
+    #long_description=readme,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.6",
